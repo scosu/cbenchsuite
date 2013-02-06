e8103cf25bb72ff6eca12e93e3f23ff28dc2acb7
Markus Pargmann <mpargmann@allfex.org>
2013-02-06 11:49:00 +0100

    data structure
    Signed-off-by: Markus Pargmann <mpargmann@allfex.org>

---
+++ include/data.h
@@ -0,0 +1,16 @@
+#ifndef _DATA_H_
+#define _DATA_H_
+
+enum data_type {
+	DATA_TYPE_MONITOR = 0x1,
+	DATA_TYPE_RESULT = 0x2,
+};
+
+struct data {
+	enum data_type type;
+	unsigned int run;
+	void *data;
+	struct list_head run_data;
+};
+
+#endif  /* _DATA_H_ */
