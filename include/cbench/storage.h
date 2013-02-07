ec734a9a56e2a705045b9513e179d2630adc187a
Markus Pargmann <mpargmann@allfex.org>
2013-02-07 21:19:27 +0100

    headers: move to subdir cbench
    Signed-off-by: Markus Pargmann <mpargmann@allfex.org>

---
+++ include/cbench/storage.h
@@ -0,0 +1,6 @@
+#ifndef _STORAGE_H_
+#define _STORAGE_H_
+
+
+
+#endif  /* _STORAGE_H_ */
---
+++ include/cbench/system.h
@@ -0,0 +1,6 @@
+#ifndef _CBENCH_SYSTEM_H_
+#define _CBENCH_SYSTEM_H_
+
+
+
+#endif  /* _CBENCH_SYSTEM_H_ */
